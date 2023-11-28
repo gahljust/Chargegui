14d584ee09c57cf37ff9b8b3f5f3afe708c8a403
gahljust <gahljust@isu.edu>
2023-11-28 11:37:34 -0700

    More modular code / Fixed int.txt

--- dose_calculator.py
+++ dose_calculator.py
@@ -8,35 +8,56 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import tempfile
 
+
+def load_data():
+    """
+    Load data from the CSV file.
+    """
+    if not filename:
+        tk.messagebox.showerror("Error", "No file selected")
+        return None
+    return pd.read_csv(filename, sep='  ', engine='python')
+
+def create_histogram(df, window):
+    """
+    Create a histogram and display it in the given window.
+    """
+    figure = plt.Figure(figsize=(6, 5), dpi=100)
+    ax = figure.add_subplot(111)
+    chart_type = FigureCanvasTkAgg(figure, window)
+    chart_type.get_tk_widget().pack()
+
+    # Scale the data from the second column (index 1)
+    scaled_data = df.iloc[:, 1] * 10**9
+    scaled_data.plot(kind='hist', ax=ax)
+    ax.set_ylabel('Count')
+    ax.set_xlabel('Charge [nC]')
+
+    return scaled_data
+
+
 def on_button_click():
     # Only proceed if file and inputs are valid
-   if filename and entry1_1.get() and entry1_2.get() and entry1_3.get() and entry2.get() and entry3.get() and entry4.get():
-        df = pd.read_csv(filename, sep='  ', engine='python')
-
+    if filename and entry1_1.get() and entry1_2.get() and entry1_3.get() and entry2.get() and entry3.get() and entry4.get():
+        df = load_data()
+        if df is None:
+            return
 
         runname = "Run"
         begshot = int(entry2.get()) - 1
         endshot = int(entry3.get())
 
-         # Average charge from entry1_2 to entry1_3
+        # Average charge from entry1_2 to entry1_3
         beg_index = int(entry1_2.get()) - 1
         end_index = int(entry1_3.get())
-        avg_charge = df['Ch3'].iloc[beg_index:end_index].mean()*10**9
+        avg_charge = df.iloc[beg_index:end_index, 1].mean() * 10**9
         print(f'Average Charge On OSL: {avg_charge}')
 
         df1 = df.iloc[begshot:endshot]
         df1.to_csv(runname + 'int.txt', index=False)
 
-        # Sum the charge for each run
-        sum_val = df1['Ch3'].sum()*10**9
-        # Average the charge for each run
-        avg_val = df1['Ch3'].mean()*10**9
-        print(f'Average Charge for Material: {avg_val}')
-        # The standard deviation of the charge for each run
-        std_val = df1['Ch3'].std()*10**9
-
         # New OSL Dose calculation
-        osl_dose = round(float(entry1_1.get()) / avg_charge,2)
+        osl_dose = round(float(entry1_1.get()) / avg_charge, 2)
         print(f'OSL Dose: {osl_dose}')
 
         # Material multiplier
@@ -52,39 +73,29 @@ def on_button_click():
             return
 
         print(f'Material Multiplier: {multiplier}')
-        # Multiply the '# Pulses', 'OSL dose', material multiplier, and the average charge
-        # result = (int(entry4.get()) * osl_dose * multiplier * avg_val)/1000000
-        result = round((int(entry4.get()) * osl_dose * multiplier * avg_charge)/1000000, 2)
-
+        result = round((int(entry4.get()) * osl_dose * multiplier * avg_charge) / 1000000, 2)
 
         # Create new window for the result and the histogram
         new_window = tk.Toplevel(root)
         new_window.title("Dose and Charge Histogram")
 
         # Display result and other details
-        result_label = tk.Label(new_window, text=f"Dose: {result} [Mrad]\nMaterial: {material}\nStart Shot: {begshot+1}\nEnd Shot: {endshot}\n# Pulses: {entry4.get()} \nDose on OSL: {osl_dose} [rad/nC]\nFile Name: {filename}")
+        result_label = tk.Label(new_window, text=f"Dose: {result} [Mrad]\nMaterial: {material}\nStart Shot: {begshot + 1}\nEnd Shot: {endshot}\n# Pulses: {entry4.get()}\nDose on OSL: {osl_dose} [rad/nC]\nFile Name: {filename}")
         result_label.pack()
 
-        # Scale the data
-        scaled_data = df1['Ch3'] * 10**9
+        # Use the new create_histogram function
+        create_histogram(df1, new_window)
 
-        # Create histogram
-        figure = plt.Figure(figsize=(6,5), dpi=100)
-        ax = figure.add_subplot(111)
-        chart_type = FigureCanvasTkAgg(figure, new_window)
-        chart_type.get_tk_widget().pack()
-        scaled_data.plot(kind='hist', ax=ax)
-        ax.set_ylabel('Count')
-        ax.set_xlabel('Charge [nC]')
 
 
 
 
 def on_button_click2():
-        # Only proceed if file and entry2 and entry3 inputs are valid
+    # Only proceed if file and entry2 and entry3 inputs are valid
     if filename and entry2.get() and entry3.get():
-
-        df = pd.read_csv(filename, sep='  ', engine='python')
+        df = load_data()  # Use the modular load_data function
+        if df is None:
+            return
 
         runname = "Run"
         begshot = int(entry2.get()) - 1
@@ -97,19 +108,10 @@ def on_button_click2():
         new_window = tk.Toplevel(root)
         new_window.title("Dose and Charge Histogram")
 
-        # Create histogram
-        figure = plt.Figure(figsize=(6,5), dpi=100)
-        ax = figure.add_subplot(111)
-        chart_type = FigureCanvasTkAgg(figure, new_window)
-        chart_type.get_tk_widget().pack()
-        
-        # Scale the data
-        scaled_data = df1['Ch3'] * 10**9
+        # Use the new create_histogram function
+        scaled_data = create_histogram(df1, new_window)
 
-        # Plot the scaled histogram
-        scaled_data.plot(kind='hist', ax=ax)
-
-        # Calculate statistics
+        # Calculate statistics using the scaled data
         mean = scaled_data.mean()
         std_dev = scaled_data.std()
         total_entries = len(scaled_data)
@@ -117,22 +119,23 @@ def on_button_click2():
         # Add statistics text to histogram
         stats_text = f"Mean: {mean:.2f} \nStd Dev: {std_dev:.2f}  \nTotal Entries: {total_entries}"
         props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        ax = scaled_data.plot(kind='hist').axes  # Get the axes from the plot
         ax.text(0.85, 1.13, stats_text, transform=ax.transAxes, fontsize=10, verticalalignment='top', bbox=props)
         ax.set_ylabel('Count')
         ax.set_xlabel('Charge [nC]')
 
 
-
 def choose_file():
     global filename
     filename = filedialog.askopenfilename()
-    # Check if selected file is a 'int.meas' file
-    if filename.endswith('int.meas'):
+    # Check if selected file is a '.meas' or '.txt' file
+    if filename.endswith('.meas') or filename.endswith('.txt'):
         temp_dir = tempfile.gettempdir()  # returns the location of temporary directory
         shutil.copy(filename, temp_dir)  # copies the file to the temp directory
         file_label.config(text=filename)  # update label with selected file path
-    # else:
-    #     tk.messagebox.showerror(title="Invalid file", message="Please select a 'int.meas' file.")
+    else:
+        # Show error if file is not of the expected type
+        tk.messagebox.showerror(title="Invalid file", message="Please select a '.meas' or '.txt' file.")
 
 root = tk.Tk()
 
